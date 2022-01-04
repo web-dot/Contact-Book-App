a013bbaafc3e8269fd51f9a919b013275a3f1c2a
sudipto <sudipto.c@heraizen.com>
2022-01-04 20:25:24 +0530

    implemented 3.Search Contact

--- src/com/heraizen/contactbook/DisplayContacts.java
+++ src/com/heraizen/contactbook/DisplayContacts.java
@@ -1,8 +1,33 @@
 package com.heraizen.contactbook;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+
 public class DisplayContacts {
     
-       public static void display() {
+       public static void display(Map map) {
            
+           if(map.isEmpty()) {
+               System.out.println("No Contacts created");
+           }
+           else {
+               Collection<Contact> mapVal = map.values();
+               Iterator<Contact> valIter = mapVal.iterator();
+               
+               System.out.println("---------------------------------------------------------------------------------------------");
+               System.out.printf("%5s %15s %15s %10s %15s %15s", "UiD", "FirstName", "LastName", "Mobile", "Email", "Workplace");
+                               //"%5s %15s %15s %15s %10s %10s"
+               System.out.println();
+               System.out.println("---------------------------------------------------------------------------------------------");
+               
+               while(valIter.hasNext()) {
+                   Contact c = valIter.next();
+                   System.out.format("%7s %13s %13s %13s %17s %13s", c.getId(), c.getFirstName(), c.getLastName(), c.getMobile(), c.getEmail(), c.getWorkplace());
+                   System.out.println();
+               } 
+           }
+              
+             
        }
 }
--- src/com/heraizen/contactbook/Switch.java
+++ src/com/heraizen/contactbook/Switch.java
@@ -1,7 +1,9 @@
 package com.heraizen.contactbook;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -31,23 +33,43 @@ public class Switch {
                 UserAdder.setId();
                 UserAdder.confirm();
                 
-                
+   
                 //contactList.add(c);
-                //contactMap.put(c.getMobile(), c);
+                contactMap.put(c.getMobile(), c);
                 
                 //System.out.println(contactMap);
                 //System.out.println(contactList);
 
                 break;
             case 2:
-                
-            default :
-                System.out.println("Invalid choice. Please enter 1 through 6 only.");
-                System.out.println();
-                DisplayMenu.display();
-                num = sc.nextInt();
-                switchIn(num);
+                DisplayContacts.display(contactMap);
                 break;
+            case 3:
+                System.out.println("Enter text to search");
+                String srchStr = sc.nextLine();
+                Contact con=null;
+                Collection<Contact> mapVal = contactMap.values();
+                Iterator<Contact> valIter = mapVal.iterator();
+                while(valIter.hasNext()) {
+                    con = valIter.next();
+                    if(con.getFirstName().contains(srchStr) || con.getLastName().contains(srchStr)||con.getMobile().contains(srchStr)) {
+                        System.out.println("---------------------------------------------------------------------------------------------");
+                        System.out.printf("%5s %15s %15s %10s %15s %15s", "UiD", "FirstName", "LastName", "Mobile", "Email", "Workplace");
+                                        //"%5s %15s %15s %15s %10s %10s"
+                        System.out.println();
+                        System.out.println("---------------------------------------------------------------------------------------------");
+                        System.out.format("%7s %13s %13s %13s %17s %13s", con.getId(), con.getFirstName(), con.getLastName(), con.getMobile(), con.getEmail(), con.getWorkplace());
+                        System.out.println();
+                    }
+                    else {
+                        System.out.println("No data found");
+                    }
+                }
+                
+                
+                
+                
+                
         }
     }
 }
